7c0573266284623859c2997c1951cca045a168cb
Dongdong <107335680+Dongdong-d@users.noreply.github.com>
2025-01-22 11:00:30 +0800

    Update coco2odvg.py

--- tools/coco2odvg.py
+++ tools/coco2odvg.py
@@ -10,7 +10,7 @@ import os
 # {"0": "person", "1": "bicycle", "2": "car", "3": "motorcycle", "4": "airplane", "5": "bus", "6": "train", "7": "truck", "8": "boat", "9": "traffic light", "10": "fire hydrant", "11": "stop sign", "12": "parking meter", "13": "bench", "14": "bird", "15": "cat", "16": "dog", "17": "horse", "18": "sheep", "19": "cow", "20": "elephant", "21": "bear", "22": "zebra", "23": "giraffe", "24": "backpack", "25": "umbrella", "26": "handbag", "27": "tie", "28": "suitcase", "29": "frisbee", "30": "skis", "31": "snowboard", "32": "sports ball", "33": "kite", "34": "baseball bat", "35": "baseball glove", "36": "skateboard", "37": "surfboard", "38": "tennis racket", "39": "bottle", "40": "wine glass", "41": "cup", "42": "fork", "43": "knife", "44": "spoon", "45": "bowl", "46": "banana", "47": "apple", "48": "sandwich", "49": "orange", "50": "broccoli", "51": "carrot", "52": "hot dog", "53": "pizza", "54": "donut", "55": "cake", "56": "chair", "57": "couch", "58": "potted plant", "59": "bed", "60": "dining table", "61": "toilet", "62": "tv", "63": "laptop", "64": "mouse", "65": "remote", "66": "keyboard", "67": "cell phone", "68": "microwave", "69": "oven", "70": "toaster", "71": "sink", "72": "refrigerator", "73": "book", "74": "clock", "75": "vase", "76": "scissors", "77": "teddy bear", "78": "hair drier", "79": "toothbrush"}
 
 def create_id_map(ori_map):
-    id_map = {int(k): i + 1 for i, (k, v) in enumerate(ori_map.items())}
+    id_map = {int(k)-1: i + 1 for i, (k, v) in enumerate(ori_map.items())}
     key_list = list(id_map.keys())
     val_list = list(id_map.values())
     return id_map, key_list, val_list
@@ -51,7 +51,7 @@ def coco2odvg(args, id_map, key_list, val_list):
                 "height": img_info["height"],
                 "width": img_info["width"],
                 "detection": {
-                    "instances": instance_list
+                "instances": instance_list
                 }
             }
         )
@@ -65,14 +65,14 @@ def create_dataset_json(root, train_odvg, train_label_map, train_mode,
                         val_coco, val_mode, output):
     train_root = os.path.join(root,'train')
     val_root = os.path.join(root,'val')
-    print(train_label_map)
-    train_label_map = str(train_label_map)
+    label_map = {str(int(k)-1): v for k, v in json.loads(train_label_map).items()}
+    print(label_map)
     dataset_info = {
         "train": [
             {
                 "root": train_root,
                 "anno": train_odvg,
-                "label_map": train_label_map,
+                "label_map": label_map,
                 "dataset_mode": train_mode
             }
         ],
@@ -91,19 +91,18 @@ def create_dataset_json(root, train_odvg, train_label_map, train_mode,
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("coco to odvg format.", add_help=True)
     parser.add_argument("--root", type=str, help="dataset root directory", 
-                        default='/workspace/Open-GroundingDino/DATASET/catdog_coco'
+                        default='DATASET/catdog_coco'
     )
-    
     parser.add_argument("--train_coco", type=str, help="train set JSON file path",
                         default='DATASET/catdog_coco/annotations/instances_train.json'
     )
-
-    parser.add_argument("--ori_map", type=str, help="original label map in JSON format", 
-                        default='{"1": "dog", "2": "cat"}'
-    )
-
+    
     parser.add_argument("--val_coco", type=str, help="validation set JSON file path", 
                         default='DATASET/catdog_coco/annotations/instances_val.json'
+    )
+    
+    parser.add_argument("--ori_map", type=str, help="original label map in JSON format", 
+                        default='{"1": "dog", "2": "cat"}'
     )
     
     parser.add_argument("--mode_train", type=str, help="training set dataset mode", 
